4b2205d4958247573330d40e148d1ce79d2d2cf7
Mackenzie Kettwig <mackenziekettwig@Mackenzies-Laptop.local>
2024-06-27 09:12:29 -0700

    Added lesson images and fixed navigation to lesson pages

---
+++ app/screens/LessonDetailScreen.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Text, Image } from 'react-native';
+import FullWidthPicture from "../../components/FullWidthPicture";
+import { doc, setDoc } from "firebase/firestore";
+import { FIREBASE_AUTH, FIRESTORE_DB } from "../../FirebaseConfig";
+import { ScrollView } from 'react-native-gesture-handler';
+
+const LessonDetailScreen = ({ route, navigation }) => {
+    const { lesson } = route.params;
+    const userId = FIREBASE_AUTH.currentUser?.uid;
+
+    const markLessonComplete = async () => {
+      if (userId) {
+        const lessonTrackingRef = doc(FIRESTORE_DB, 'users', userId, 'lessonTracking', 'progress');
+        const updatedProgress = { [lesson.id]: true };
+        await setDoc(lessonTrackingRef, updatedProgress, { merge: true });
+        console.log(`Marking ${lesson.title} as complete`);
+        navigation.goBack();
+      } else {
+        console.error("User ID is undefined.");
+      }
+    };
+
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollViewStyle}>
+          <FullWidthPicture uri={Image.resolveAssetSource(lesson.image).uri} />
+        </ScrollView>
+        <TouchableOpacity onPress={markLessonComplete} style={styles.doneBtn}>
+            <Text style={styles.doneText}>Done</Text>
+        </TouchableOpacity>
+      </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFF', // Set background color to white
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    scrollViewStyle: {
+      flex: 1,
+      width: '100%',
+      paddingTop: 30, // Add padding at the top to shift the image down
+    },
+    doneText: {
+        fontWeight: 'bold',
+        color: '#fff',
+        fontSize: 16,
+      },
+    doneBtn: {
+        width: '80%',
+        backgroundColor: '#52796F',
+        borderRadius: 25,
+        height: 40, // Reduce the height of the button
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10, // Reduce the top margin
+        marginBottom: 10, // Reduce the bottom margin if needed
+    }
+});
+
+export default LessonDetailScreen;
--- app/screens/LessonTrackingScreen.tsx
+++ app/screens/LessonTrackingScreen.tsx
@@ -1,112 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-// import { ScrollView, View, Text, Button, StyleSheet, Image } from 'react-native';
-// import { useNavigation } from '@react-navigation/native';
-// import { FIREBASE_AUTH, FIRESTORE_DB } from '../../FirebaseConfig';
-// import { doc, getDoc, setDoc } from 'firebase/firestore';
-// import FullWidthPicture from '../../components/FullWidthPicture';
-
-// const lessons = [
-//   {
-//     id: 1,
-//     image: require('../../assets/lesson1.png'),
-//   },
-//   {
-//     id: 2,
-//     image: require('../../assets/lesson1.png'),
-//   },
-//   // Add more lessons as needed
-// ];
-
-// const LessonTrackingScreen = () => {
-//   const [currentLesson, setCurrentLesson] = useState(null);
-//   const [userProgress, setUserProgress] = useState({});
-//   const navigation = useNavigation();
-//   const userId = FIREBASE_AUTH.currentUser?.uid;
-
-//   useEffect(() => {
-//     const fetchUserProgress = async () => {
-//       if (userId) {
-//         const lessonTrackingRef = doc(FIRESTORE_DB, 'users', userId, 'lessonTracking', 'progress');
-//         const progressDoc = await getDoc(lessonTrackingRef);
-//         let data = {};
-
-//         if (progressDoc.exists()) {
-//           data = progressDoc.data();
-//         } else {
-//           // Initialize progress if it doesn't exist
-//           const initialProgress = lessons.reduce((acc, lesson) => {
-//             acc[lesson.id] = false; // Set all lessons to incomplete
-//             return acc;
-//           }, {});
-//           await setDoc(lessonTrackingRef, initialProgress);
-//           data = initialProgress;
-//         }
-
-//         setUserProgress(data);
-//         // Find the first incomplete lesson
-//         const nextLesson = lessons.find(lesson => !data[lesson.id]);
-//         setCurrentLesson(nextLesson);
-//       }
-//     };
-
-//     fetchUserProgress();
-
-//     navigation.setOptions({
-//       headerRight: () => (
-//         currentLesson ? (
-//           <Button
-//             onPress={() => markLessonComplete(currentLesson.id)}
-//             title="Done"
-//             color="#000"  // Set your preferred color
-//           />
-//         ) : null
-//       )
-//     });
-//   }, [userId, currentLesson, navigation]);
-
-//   const markLessonComplete = async (lessonId) => {
-//     if (userId) {
-//       const lessonTrackingRef = doc(FIRESTORE_DB, 'users', userId, 'lessonTracking', 'progress');
-//       const newProgress = { ...userProgress, [lessonId]: true };
-//       await setDoc(lessonTrackingRef, newProgress);
-//       setUserProgress(newProgress);
-
-//       const nextLesson = lessons.find(lesson => !newProgress[lesson.id]);
-//       setCurrentLesson(nextLesson);
-//     }
-//   };
-
-//   return (
-//     <ScrollView style={styles.container}>
-//       {currentLesson ? (
-//         <>
-//           <FullWidthPicture uri={Image.resolveAssetSource(currentLesson.image).uri} />
-//         </>
-//       ) : (
-//         <Text style={styles.completedText}>All lessons completed!</Text>
-//       )}
-//     </ScrollView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff', // Ensures the background is white
-//     paddingTop: 0 // Ensures content is flush to the top
-//   },
-//   completedText: {
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//     marginTop: 20
-//   }
-// });
-
-// export default LessonTrackingScreen;
-
 import React, { useEffect, useState } from 'react';
-import { ScrollView, View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { FIREBASE_AUTH, FIRESTORE_DB } from '../../FirebaseConfig';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
@@ -115,18 +8,66 @@ const lessons = [
   {
     id: 1,
     title: 'Lesson #1: Sleep Efficiency',
-    image: require('../../assets/lesson1.png'),
+    image: require('../../assets/sleep_study_module_1.png'),
   },
   {
     id: 2,
-    title: 'Lesson #2: Sleep Quality',
-    image: require('../../assets/lesson1.png'),
+    title: 'Lesson #2: Sleep Schedule and Restriction',
+    image: require('../../assets/sleep_study_module_2.png'),
   },
-  // Add more lessons as needed
+  {
+    id: 3,
+    title: 'Lesson #3: Keeping Track',
+    image: require('../../assets/sleep_study_module_3.png'),
+  },
+  {
+    id: 4,
+    title: 'Lesson #4: Naps',
+    image: require('../../assets/sleep_study_module_4.png'),
+  },
+  {
+    id: 5,
+    title: 'Lesson #5: Lights and Sounds',
+    image: require('../../assets/sleep_study_module_5.png'),
+  },
+  {
+    id: 6,
+    title: 'Lesson #6: Bedtime Routines',
+    image: require('../../assets/sleep_study_module_6.png'),
+  },
+  {
+    id: 7,
+    title: 'Lesson #7: Healthy Eating',
+    image: require('../../assets/sleep_study_module_7.png'),
+  },
+  {
+    id: 8,
+    title: 'Lesson #8: Healthy Drinks',
+    image: require('../../assets/sleep_study_module_8.png'),
+  },
+  {
+    id: 9,
+    title: 'Lesson #9: Healthy Body Weight',
+    image: require('../../assets/sleep_study_module_9.png'),
+  },
+  {
+    id: 10,
+    title: 'Lesson #10: Building Physical Activity and Strength',
+    image: require('../../assets/sleep_study_module_10.png'),
+  },
+  {
+    id: 11,
+    title: 'Lesson #11: Stress',
+    image: require('../../assets/sleep_study_module_11.png'),
+  },
+  {
+    id: 12,
+    title: 'Lesson #12: Cognitive Strategies to Improve Sleep',
+    image: require('../../assets/sleep_study_module_12.png'),
+  },
 ];
 
 const LessonTrackingScreen = () => {
-  const [currentLesson, setCurrentLesson] = useState(null);
   const [userProgress, setUserProgress] = useState({});
   const [allLessonsCompleted, setAllLessonsCompleted] = useState(false);
   const navigation = useNavigation();
@@ -137,60 +78,36 @@ const LessonTrackingScreen = () => {
   }, [userId]);
 
   const fetchUserProgress = async () => {
-    if (userId) {
-      const lessonTrackingRef = doc(FIRESTORE_DB, 'users', userId, 'lessonTracking', 'progress');
-      const progressDoc = await getDoc(lessonTrackingRef);
-      let data = {};
-
-      if (progressDoc.exists()) {
-        data = progressDoc.data();
-      } else {
-        const initialProgress = lessons.reduce((acc, lesson) => ({ ...acc, [lesson.id]: false }), {});
-        await setDoc(lessonTrackingRef, initialProgress);
-        data = initialProgress;
-      }
-
+    const lessonTrackingRef = doc(FIRESTORE_DB, 'users', userId, 'lessonTracking', 'progress');
+    const progressDoc = await getDoc(lessonTrackingRef);
+    if (progressDoc.exists()) {
+      const data = progressDoc.data();
       setUserProgress(data);
-      checkLessonsCompletion(data);
-    }
-  };
-
-  const checkLessonsCompletion = (data) => {
-    const allDone = Object.values(data).every(status => status);
-    setAllLessonsCompleted(allDone);
-    if (!allDone) {
-      const nextLesson = lessons.find(lesson => !data[lesson.id]);
-      setCurrentLesson(nextLesson);
-    }
-  };
-
-  const markLessonComplete = async (lessonId) => {
-    if (userId) {
-      const lessonTrackingRef = doc(FIRESTORE_DB, 'users', userId, 'lessonTracking', 'progress');
-      const newProgress = { ...userProgress, [lessonId]: true };
-      await setDoc(lessonTrackingRef, newProgress);
-      setUserProgress(newProgress);
-      checkLessonsCompletion(newProgress);
+      const allCompleted = Object.keys(data).length === lessons.length && Object.values(data).every(status => status);
+      setAllLessonsCompleted(allCompleted);
+    } else {
+      // Set initial false for each lesson if no progress document exists
+      const initialProgress = lessons.reduce((acc, lesson) => ({ ...acc, [lesson.id]: false }), {});
+      await setDoc(lessonTrackingRef, initialProgress);
+      setUserProgress(initialProgress);
+      setAllLessonsCompleted(false);
     }
   };
 
   return (
     <ScrollView style={styles.container}>
       {allLessonsCompleted ? (
-        <View>
-          {lessons.map((lesson) => (
-            <TouchableOpacity key={lesson.id} style={styles.lessonItem} onPress={() => setCurrentLesson(lesson)}>
-              <Text style={styles.lessonTitle}>{lesson.title}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
-      ) : currentLesson ? (
-        <View>
-          <Text style={styles.title}>{currentLesson.title}</Text>
-          <Button title="Mark as Done" onPress={() => markLessonComplete(currentLesson.id)} />
-        </View>
+        <Text style={styles.completedText}>All lessons completed!</Text>
       ) : (
-        <Text style={styles.completedText}>Loading lessons...</Text>
+        lessons.map((lesson) => (
+          <TouchableOpacity
+            key={lesson.id}
+            style={styles.lessonItem}
+            onPress={() => navigation.navigate('LessonDetailScreen', { lesson })}
+          >
+            <Text style={styles.lessonTitle}>{lesson.title}</Text>
+          </TouchableOpacity>
+        ))
       )}
     </ScrollView>
   );
@@ -201,24 +118,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
+  lessonTitle: {
+    fontSize: 18,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
   completedText: {
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 20,
-  },
-  lessonItem: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  lessonTitle: {
-    fontSize: 18,
   },
 });
 
